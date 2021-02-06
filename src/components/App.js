d9515f85f0c3a24de66d0978a6760ff89eb50294
Noura <contact.nouraloudani@gmail.com>
2021-02-05 22:43:22 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import Question from "./Question";
 import quiz from "../data/quiz";
 
 function App() {
-  const [questions, setQuestions] = useState(quiz);
-  const [currentQuestionId, setCurrentQuestion] = useState(1);
+  const [questions, setQuestions] = useState(quiz); 
+  const [currentQuestionId, setCurrentQuestion] = useState(1); //cq
   const [score, setScore] = useState(0);
-  const currentQuestion = questions.find((q) => q.id === currentQuestionId);
+  const currentQuestion = questions.find((q) => q.id === currentQuestionId); //cq
 
   function handleQuestionAnswered(correct) {
     if (currentQuestionId < questions.length) {
